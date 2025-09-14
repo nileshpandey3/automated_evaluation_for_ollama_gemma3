2467c99c81529f8c992a36a6145cd76ec0bf6c1e
Nilesh Pandey <nileshpandey3@gmail.com>
2025-09-13 19:02:46 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-### Test Automation using deepeval library on a Pytest based framework 
-### using a locally running gemma3:1b model with the help of Ollama: Local LLM Management Framework
+### Test Automation using deepeval and Pytest 
+
+### Features:
+- Test Automation using deepeval and Pytest using a locally running gemma3:1b model on Ollama which is a Local LLM Management Framework
+- A simple automated test suite which verifies LLM output for some basic factual questions
+- Used some base LLM evaluation metrics like `AnswerRelevancy` , `Bias` and `Toxicity`
+
+
+### How to Run tests?
+- Clone the project 
+- Download and install Ollama tool and gemma3:1b https://ollama.com/library/gemma3 model
+- Make sure your locall ollama instance is up and running the gemma3 model 
+- Run either `pytest -s --html report.html` OR `deepeval test run ./`
+
+### Example Pytest Report
+![Test Report Screenshot](html_report_screenshot.png)
+
+
+### References:
+- https://deepeval.com/docs/getting-started
+- https://github.com/ollama/ollama
--- tests/test_gemma3_knowledge.py
+++ tests/test_gemma3_knowledge.py
@@ -1,15 +1,18 @@
 import pytest
-from deepeval.test_case import LLMTestCase
-from deepeval import evaluate
-from setup import ollama_client, answer_relevancy,bias, toxicity
 from deepeval import assert_test
+from deepeval.test_case import LLMTestCase
+from setup import ollama_client, answer_relevancy,bias, toxicity
+
+
+# Choose some non RAG based metrics to be used for the test cases
+metrics = [answer_relevancy, bias, toxicity]
+
 
 @pytest.mark.arithmetic
 def test_arithmetic_question():
-    input = 'What is 2+2?'
-    response = ollama_client.prompt(input=input)
-    test_case=LLMTestCase(input=input, actual_output=str(response))
-    metrics = [answer_relevancy, bias, toxicity]
+    input_question = 'What is 2+2?'
+    response = ollama_client.prompt(input=input_question)
+    test_case=LLMTestCase(input=input_question, actual_output=str(response))
     try:
         assert_test(test_case=test_case, metrics=metrics)
     except AssertionError as e:
@@ -17,10 +20,9 @@ def test_arithmetic_question():
 
 @pytest.mark.science
 def test_science_question():
-    input = "What is the standard boiling point temperature of water in degrees celcius?"
-    response = ollama_client.prompt(input=input)
-    test_case=LLMTestCase(input=input, actual_output=str(response))
-    metrics = [answer_relevancy, bias, toxicity]
+    input_question = "What is the standard boiling point temperature of water in degrees celcius?"
+    response = ollama_client.prompt(input=input_question)
+    test_case=LLMTestCase(input=input_question, actual_output=str(response))
     try:
         assert_test(test_case=test_case, metrics=metrics)
     except AssertionError as e:
@@ -28,12 +30,11 @@ def test_science_question():
 
 @pytest.mark.literature
 def test_literature_question():
-    input = "Who wrote Julius Ceaser?"
-    response = ollama_client.prompt(input=input)
-    test_case=LLMTestCase(input=input, actual_output=str(response))
-    metrics = [answer_relevancy,bias, toxicity]
+    input_question = "Who wrote Julius Ceaser?"
+    response = ollama_client.prompt(input=input_question)
+    test_case=LLMTestCase(input=input_question, actual_output=str(response))
     try:
         assert_test(test_case=test_case, metrics=metrics)
     except AssertionError as e:
         print(str(e))
-
+        
